8c3b4326897e242827a6d97b731157590be11dd4
Talon Durrant <talonmdurrant@gmail.com>
2022-01-04 21:18:51 -0500

    creating configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,56 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gibby-floral-v2",
+    title: 'Gibby Floral | Occasions, Home, and Garden since 1956',
+    description:
+      'We have been providing the community with beautiful floral designs since 1956, and we’re excited to continue to help you with your special occasions, weddings, memorials, and just something special for the one you love.',
+    siteUrl: 'https://www.gibbyfloral.com',
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
+    'gatsby-plugin-netlify-cms',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingId: "G-W3E3TXJMVB",
+        trackingIds: [process.env.GA_TRACKING_ID],
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'posts',
+        path: './content/posts/',
       },
-      __key: "pages",
+      __key: 'posts',
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './content/pages/',
+      },
+      __key: 'pages',
+    },
+    'gatsby-plugin-postcss',
   ],
 };
