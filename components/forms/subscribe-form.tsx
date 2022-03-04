79f23e30cdb339de22353a43281b96841b4e132b
Talon Durrant <talonmdurrant@gmail.com>
2022-03-04 18:37:44 -0500

    updated form to not render multiple

--- components/forms/subscribe-form.tsx
+++ components/forms/subscribe-form.tsx
@@ -1,20 +1,27 @@
-import { useEffect } from 'react';
+import { Component } from 'react';
 
-const SubscribeForm = () => {
-  useEffect(() => {
+class SubscribeForm extends Component {
+  componentDidMount() {
     const script = document.createElement('script');
 
+    script.id = 'active-campaign-form';
     script.src = 'https://botanicalsdesign.activehosted.com/f/embed.php?id=13';
     script.async = true;
 
     document.body.appendChild(script);
+  }
 
-    return () => {
+  componentWillUnmount() {
+    const script = document.querySelector('#active-campaign-form');
+
+    if (script) {
       document.body.removeChild(script);
-    };
-  });
+    }
+  }
 
-  return <div className="_form_13"></div>;
-};
+  render() {
+    return <div className="_form_13"></div>;
+  }
+}
 
 export default SubscribeForm;
